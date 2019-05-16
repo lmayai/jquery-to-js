71eb927ab422d42afc0c00099e6b5a038868fa02
lmayai <leonardomayairegui@gmail.com>
2019-05-16 13:21:45 -0500

    Uso de promesas en JS

--- README.md
+++ README.md
@@ -3,3 +3,66 @@ Project Platzi Video website created using Jquery and also a similiar with JS va
 
 ### Objectives
 Created site using vanilla Js and compare it with Jquery
+
+### Procesos asíncronos
+Un proceso asíncrono es un código que se está ejecutando, pero no ha terminado de ejecutarse antes de que se ejecute un código que está después de él.
+
+Esto permite que la aplicación no se cuelque mientras está ejecutando un proceso muy largo.
+
+### Variables
+var es la forma de crear variables hasta ES5.
+const es para declarar constantes.
+let es para crear variables que cambian.
+
+### Funciones
+```js
+functioncambiarNombre(nuevoNombre) {
+  cambia = nuevoNombre;
+}
+
+//Desde EC6, las funciones se pueden declarar como arrow functions.
+cambiarNombre = (nuevoNombre) => {
+  cambia = nuevoNombre;
+}
+```
+
+### Promesas
+Una promesa es un objeto que representa la terminación o el fracaso eventual de una operación asíncrona.
+```js
+//Crear una promesa
+const getUser = new Promise(function(todoBien, todoMal) {
+  //llamar a un API
+  todoBien("todo bien");
+})
+
+//Consumir una promesa
+getUser
+  .then(function(msn) { 
+    //maneja cuando la promesa funciona correctamente.
+  })
+  .catch(function(msn) {
+    //maneja cuando hay un error en la promesa.
+  })
+
+//Consumir varias promesas a la vez.
+//El then se ejecutan cuando terminen todas las promesas.
+//El catch se ejecuta en el primer error.
+Promise.all([
+  promesa1,
+  promesa2
+])
+.then(function() {})
+.catch(function() {})
+
+//Se ejecuta el then de la promesa que termine primero.
+Promise.race([
+  promesa1,
+  promesa2
+])
+.then(function() {})
+.catch(function() {})
+```
+### Timers
+setInterval() se ejecuta cada cierto tiempo.
+setTimeout() se ejecuta una sola vez luego de un periodo de tiempo.
+* Los tiempos se asignan en ms
--- src/js/home.js
+++ src/js/home.js
@@ -1,262 +1,67 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+console.log('Hi ET')
 
-let cambia = "@LeonidasEsteban"
+/**
+ * VARIABLES
+ * const: no cambia y no se puede cambiar
+ * let: cambia
+*/
+let cambia = 'Cambia'
+const noCambia = 'No cambia'
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
+function cambiaNombre(nombre) {
+    cambia = nombre //Nombre es un parámetro de la función
 }
 
-const getUserAll = new Promise(function(todoBien, todoMal) {
-  // llamar a un api
-  setTimeout(function() {
-    // luego de 3 segundos
-    todoBien('se acabó el tiempo');
-  }, 5000)
+/**
+ * Promeses
+ * Se asigna a una variable y recibe como param una función.
+ * La funcion reciba a su vez 2 params: ok or bad.
+ * Ejecuto todo bien si, está bien
+ */
+const getUser1 = new Promise( function(todoBien,todoMal){
+    // Llamar a un API (demora cierto tiempo)
+    setTimeout(function(){
+        todoMal('Se acabo el tiempo') //Para que falle la promesa
+    },3000) //
 })
-
-const getUser = new Promise(function(todoBien, todoMal) {
-  // llamar a un api
-  setTimeout(function() {
-    // luego de 3 segundos
-    todoBien('se acabó el tiempo 3');
-  }, 3000)
-})
-
-// getUser
-//   .then(function() {
-//     console.log('todo está bien en la vida')
-//   })
-//   .catch(function(message) {
-//     console.log(message)
-//   })
-
-Promise.race([
-  getUser,
-  getUserAll,
-])
-.then(function(message) {
-  console.log(message);
-})
-.catch(function(message) {
-  console.log(message)
-})
-
-
-
-$.ajax('https://randomuser.me/api/sdfdsfdsfs', {
-  method: 'GET',
-  success: function(data) {
-    console.log(data)
-  },
-  error: function(error) {
-    console.log(error)
-  }
-})
-
-fetch('https://randomuser.me/api/dsfdsfsd')
-  .then(function (response) {
-    // console.log(response)
-    return response.json()
-  })
-  .then(function (user) {
-    console.log('user', user.results[0].name.first)
-  })
-  .catch(function() {
-    console.log('algo falló')
-  });
-
-
-(async function load() {
-  // await
-  // action
-  // terror
-  // animation
-  async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.data.movie_count > 0) {
-      // aquí se acaba
-      return data;
-    }
-    // si no hay pelis aquí continua
-    throw new Error('No se encontró ningun resultado');
-  }
-  const $form = document.getElementById('form');
-  const $home = document.getElementById('home');
-  const $featuringContainer = document.getElementById('featuring');
-
-
-  function setAttributes($element, attributes) {
-    for (const attribute in attributes) {
-      $element.setAttribute(attribute, attributes[attribute]);
-    }
-  }
-  const BASE_API = 'https://yts.am/api/v2/';
-
-  function featuringTemplate(peli) {
-    return (
-      `
-      <div class="featuring">
-        <div class="featuring-image">
-          <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
-        </div>
-        <div class="featuring-content">
-          <p class="featuring-title">Pelicula encontrada</p>
-          <p class="featuring-album">${peli.title}</p>
-        </div>
-      </div>
-      `
-    )
-  }
-
-  $form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    $home.classList.add('search-active')
-    const $loader = document.createElement('img');
-    setAttributes($loader, {
-      src: 'src/images/loader.gif',
-      height: 50,
-      width: 50,
+/**
+ * Ahora al llamar a mi promesa, llamo a then si esta bien
+ */
+getUser1.
+    then(function(){
+        console.log('Todo muy bien en la promesa')
     })
-    $featuringContainer.append($loader);
-
-    const data = new FormData($form);
-    try {
-      const {
-        data: {
-          movies: pelis
-        }
-      } = await getData(`${BASE_API}list_movies.json?limit=1&query_term=${data.get('name')}`)
-
-      const HTMLString = featuringTemplate(pelis[0]);
-      $featuringContainer.innerHTML = HTMLString;
-    } catch(error) {
-      alert(error.message);
-      $loader.remove();
-      $home.classList.remove('search-active');
-    }
-  })
-
-  function videoItemTemplate(movie, category) {
-    return (
-      `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category=${category}>
-        <div class="primaryPlaylistItem-image">
-          <img src="${movie.medium_cover_image}">
-        </div>
-        <h4 class="primaryPlaylistItem-title">
-          ${movie.title}
-        </h4>
-      </div>`
-    )
-  }
-  function createTemplate(HTMLString) {
-    const html = document.implementation.createHTMLDocument();
-    html.body.innerHTML = HTMLString;
-    return html.body.children[0];
-  }
-  function addEventClick($element) {
-    $element.addEventListener('click', () => {
-      // alert('click')
-      showModal($element)
+    .catch(function(msg){
+        console.log('Todo mal dentro de la promesa:',msg)
     })
-  }
-  function renderMovieList(list, $container, category) {
-    // actionList.data.movies
-    $container.children[0].remove();
-    list.forEach((movie) => {
-      const HTMLString = videoItemTemplate(movie, category);
-      const movieElement = createTemplate(HTMLString);
-      $container.append(movieElement);
-      const image = movieElement.querySelector('img');
-      image.addEventListener('load', (event) => {
-        event.srcElement.classList.add('fadeIn');
-      })
-      addEventClick(movieElement);
-    })
-  }
 
-  async function cacheExist(category) {
-    const listName = `${category}List`;
-    const cacheList = window.localStorage.getItem(listName);
+const getUser2 = new Promise( function(todoBien,todoMal){
+    // Llamar a un API (demora cierto tiempo)
+    setTimeout(function(){
+        todoBien('Se acabo el tiempo') //Para que falle la promesa
+    },5000) //
+})
 
-    if (cacheList) {
-      return JSON.parse(cacheList);
-    }
-    const { data: { movies: data } } = await getData(`${BASE_API}list_movies.json?genre=${category}`)
-    window.localStorage.setItem(listName, JSON.stringify(data))
-
-    return data;
-  }
-
-  // const { data: { movies: actionList} } = await getData(`${BASE_API}list_movies.json?genre=action`)
-  const actionList = await cacheExist('action');
-  // window.localStorage.setItem('actionList', JSON.stringify(actionList))
-  const $actionContainer = document.querySelector('#action');
-  renderMovieList(actionList, $actionContainer, 'action');
-
-  const dramaList = await await cacheExist('drama');
-  const $dramaContainer = document.getElementById('drama');
-  renderMovieList(dramaList, $dramaContainer, 'drama');
-
-  const animationList = await await cacheExist('animation');
-  const $animationContainer = document.getElementById('animation');
-  renderMovieList(animationList, $animationContainer, 'animation');
+const getUser3 = new Promise( function(todoBien,todoMal){
+    // Llamar a un API (demora cierto tiempo)
+    setTimeout(function(){
+        todoBien('Good') //Para que falle la promesa
+    },3000) //
+})
 
 
+/**
+ * REcibe una lista de promesas
+ */
+Promise.all([getUser2,getUser3])
+    .then( () => console.log('GOOD'))
+    .catch( msg => console.log('BADD', msg))
 
+Promise.all([getUser2,getUser3])
+    .then( function() { console.log('GOOD2 ') })
+    .catch( function (msg) {console.log('BADD2', msg)})    
 
-
-
-
-
-  // const $home = $('.home .list #item');
-  const $modal = document.getElementById('modal');
-  const $overlay = document.getElementById('overlay');
-  const $hideModal = document.getElementById('hide-modal');
-
-  const $modalTitle = $modal.querySelector('h1');
-  const $modalImage = $modal.querySelector('img');
-  const $modalDescription = $modal.querySelector('p');
-
-  function findById(list, id) {
-    return list.find(movie => movie.id === parseInt(id, 10))
-  }
-
-  function findMovie(id, category) {
-    switch (category) {
-      case 'action' : {
-        return findById(actionList, id)
-      }
-      case 'drama' : {
-        return findById(dramaList, id)
-      }
-      default: {
-        return findById(animationList, id)
-      }
-    }
-  }
-
-  function showModal($element) {
-    $overlay.classList.add('active');
-    $modal.style.animation = 'modalIn .8s forwards';
-    const id = $element.dataset.id;
-    const category = $element.dataset.category;
-    const data = findMovie(id, category);
-
-    $modalTitle.textContent = data.title;
-    $modalImage.setAttribute('src', data.medium_cover_image);
-    $modalDescription.textContent = data.description_full
-  }
-
-  $hideModal.addEventListener('click', hideModal);
-  function hideModal() {
-    $overlay.classList.remove('active');
-    $modal.style.animation = 'modalOut .8s forwards';
-
-  }
-
-
-
-
-})()
+//Con race solo devuelve la promesa que se resuelva primero
+Promise.race([getUser2,getUser3])
+    .then( function() { console.log('GOOD3 ') })
+    .catch( function (msg) {console.log('BADD3', msg)})    
